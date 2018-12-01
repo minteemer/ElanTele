c2886abe0d7824f7f6fd669c430cd0cdbafd48cb
Ilgizar <ilgizar1313@gmail.com>
2018-12-01 19:40:50 +0300

    Merge branch 'master' of /home/ilgizar/IdeaProjects/ElanTele with conflicts.

---
+++ src/main/kotlin/ir/values/DictValue.kt
@@ -0,0 +1,11 @@
+package ir.values
+
+class DictValue(val values: Map<String, Value>) : Value(ValueClass.DICT) {
+
+    override fun add(other: Value): Value = when (other) {
+        is DictValue -> DictValue(values + other.values)
+        else -> super.add(other)
+    }
+
+    override fun toString(): String = values.toList().joinToString(prefix = "[", postfix = "]")
+}
