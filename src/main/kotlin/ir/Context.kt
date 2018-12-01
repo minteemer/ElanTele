99a66c1637e26a499146b952ba8dbdb240df96fa
novo <ecat3@yandex.ru>
2018-12-01 20:24:46 +0300

    multilevel context implementation

--- src/main/kotlin/ir/Context.kt
+++ src/main/kotlin/ir/Context.kt
@@ -1,15 +1,18 @@
 package ir
 
 import ir.values.Value
-import java.lang.Exception
 
-class Context {
+class Context(private val parentContext: Context? = null) {
     private val values = HashMap<String, Value>()
 
-    fun getValue(reference: String): Value = values[reference]
-            ?: throw Exception() // TODO: Handle unresolved reference
+    fun getValue(reference: String): Value =
+            values[reference]
+                    ?: parentContext?.getValue(reference)
+                    ?: throw ContextException("Name $reference not found in context")
 
     fun setValue(reference: String, value: Value) {
         values[reference] = value
     }
+
+    fun getChildContext() = Context(this)
 }
