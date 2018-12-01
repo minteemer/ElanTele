136881db567b411f5691008b89a03a04f053598e
novo <ecat3@yandex.ru>
2018-12-01 20:41:00 +0300

    write to parent context implementation

--- src/main/kotlin/ir/Context.kt
+++ src/main/kotlin/ir/Context.kt
@@ -11,7 +11,17 @@ class Context(private val parentContext: Context? = null) {
                     ?: throw ContextException("Name $reference not found in context")
 
     fun setValue(reference: String, value: Value) {
+        if (!propagateValue(reference, value)) return
         values[reference] = value
+    }
+
+    private fun propagateValue(reference: String, value: Value): Boolean {
+        if (values.containsKey(reference)) {
+            values[reference] = value
+            return true
+        } else {
+            return parentContext?.propagateValue(reference, value) ?: return false
+        }
     }
 
     fun getChildContext() = Context(this)
