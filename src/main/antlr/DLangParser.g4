36f029cb951f3df3cc9dee5eebc67ef0c3911d4e
arsee <kuzmin.arsee@yandex.ru>
2018-12-01 15:31:35 +0300

    Error messages

---
+++ src/main/antlr/DLangParser.g4
@@ -0,0 +1,139 @@
+parser grammar DLangParser;
+
+options { tokenVocab = DLangLexer; }
+
+program
+    : NL* (statement (NL)*)+ EOF
+    ;
+
+declaration
+    : VAR variableDefinition (COMMA variableDefinition)*
+    ;
+
+variableDefinition
+    : Identifier (ASSIGNMENT expression)?
+    ;
+
+statement
+    :declaration
+    |assignment
+    |if_expression
+    |loop
+    |return_expression
+    |print
+    ;
+
+assignment
+    : reference ASSIGNMENT expression
+    ;
+
+if_expression
+    : IF expression THEN body (ELSE body) END
+    ;
+
+loop
+    : (WHILE expression NL* LOOP NL* body NL*  END)
+    | (FOR (Identifier IN)? (expression RANGE expression)? NL* LOOP NL* body NL* END)
+    ;
+
+return_expression
+    : RETURN expression?
+    ;
+
+print
+    :PRINT expression (COMMA expression)?
+    ;
+
+expression
+    : relation ( (OR|AND|XOR) relation)*
+    ;
+
+relation
+    : factor ((LE|GE|EXCL_EQ|LANGLE|RANGLE|EQEQ) factor)?
+    ;
+
+factor
+    : term ((ADD|SUB) term)*
+    ;
+
+term
+    : unary ((MULT|DIV) unary)*
+    ;
+
+unary
+    : reference
+    | reference IS typeIndicator
+    | (ADD|SUB|NOT)? primary
+    ;
+
+primary
+    : literal
+    | ReadInt
+    | ReadReal
+    | ReadString
+    | functionLiteral
+    | LPAREN expression RPAREN
+    ;
+typeIndicator
+    : IntType
+    | RealType
+    | StringType
+    | BoolType
+    | EmptyType
+    | ArrayType
+    | TupleType
+    | FUNC
+    ;
+
+functionLiteral
+    : FUNC (LPAREN Identifier (COMMA Identifier)* RPAREN)? funBody
+    ;
+
+funBody
+    : IS body END
+    | DOUBLE_ARROW expression
+    ;
+
+reference
+    :Identifier
+    |reference LSQUARE expression RSQUARE
+    |reference LPAREN expression (COMMA expression)* RPAREN
+    |reference DOT Identifier
+    ;
+
+literal
+    : IntegerLiteral
+    | RealLiteral
+    | lineStringLiteral
+    | BooleanLiteral
+    | tupple
+    | array
+    | EmptyType
+    ;
+lineStringLiteral
+    : QUOTE_OPEN (lineStringContent | lineStringExpression)* QUOTE_CLOSE
+    ;
+lineStringContent
+    : LineStrText
+    | LineStrEscapedChar
+
+    ;
+
+lineStringExpression
+    : LineStrExprStart expression RCURL
+    ;
+
+array
+    : LSQUARE (expression (COMMA expression)*)? RSQUARE
+    ;
+tupple
+    : LCURL tuppleElement (COMMA tuppleElement)* RCURL
+    ;
+tuppleElement
+    : Identifier (ASSIGNMENT expression)?
+    ;
+body
+    : (declaration
+    | statement
+    | expression)*
+    ;
--- src/main/kotlin/ir/values/Value.kt
+++ src/main/kotlin/ir/values/Value.kt
@@ -7,45 +7,45 @@ open class Value {
             throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
 
     open fun subtract(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not subtract ${other.javaClass.simpleName} from ${javaClass.simpleName}")
 
     open fun unaryMinus(): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not make negative ${javaClass.simpleName}")
 
     open fun multiply(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not multiply ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun divide(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not divide  ${javaClass.simpleName} by ${other.javaClass.simpleName}")
 
     open fun equals(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not match ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun greater(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply > operation to ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun less(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply < operation to  ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun greaterOrEqual(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply >= operation to  ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun lessOrEqual(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply <= operation to  ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun notEquals(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply /= operation to  ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun and(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply 'and' logical operation to ${javaClass.simpleName} and ${other.javaClass.simpleName}")
 
     open fun or(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply 'or' logical operation to ${javaClass.simpleName} to ${other.javaClass.simpleName}")
 
     open fun xor(other: Value): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName} to ${other.javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply 'xor' logical operation to ${javaClass.simpleName} to ${other.javaClass.simpleName}")
 
     open fun not(): Value =
-            throw UnresolvedOperatorException("Can not add ${javaClass.simpleName}")
+            throw UnresolvedOperatorException("Can not apply 'not' logical operation to ${javaClass.simpleName}")
 
 }
