cef64f345bc3a465c5e924bbfd7f14c6a9abd97a
arsee <kuzmin.arsee@yandex.ru>
2018-12-01 15:51:25 +0300

    BooleanValue class implemented

--- src/main/kotlin/ir/values/BooleanValue.kt
+++ src/main/kotlin/ir/values/BooleanValue.kt
@@ -1,3 +1,23 @@
 package ir.values
 
-class BooleanValue(val value: Boolean) : Value()
+class BooleanValue(val value: Boolean) : Value(){
+    override fun and(other: Value): Value = when(other){
+        is BooleanValue -> BooleanValue(value and other.value)
+        else -> super.and(other)
+    }
+
+    override fun or(other: Value): Value = when(other){
+        is BooleanValue -> BooleanValue(value or other.value)
+        else -> super.or(other)
+    }
+    override fun xor(other: Value): Value = when(other){
+        is BooleanValue -> BooleanValue(value xor other.value)
+        else -> super.xor(other)
+    }
+
+    override fun equals(other: Value): Value = when(other){
+        is BooleanValue -> BooleanValue(value == other.value)
+        else -> super.equals(other)
+    }
+}
+
