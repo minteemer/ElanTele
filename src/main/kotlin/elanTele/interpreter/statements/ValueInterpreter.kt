fa96abcd85bfddbbb14ee386e144cd9935bcad82
novo <ecat3@yandex.ru>
2018-12-02 20:25:54 +0300

    ValueInterpreter prototype

--- src/main/kotlin/elanTele/interpreter/statements/ValueInterpreter.kt
+++ src/main/kotlin/elanTele/interpreter/statements/ValueInterpreter.kt
@@ -1,8 +1,11 @@
 package elanTele.interpreter.statements
 
 import elanTele.ir.values.Value
-import elanTele.ir.values.classes.IntegerValue
-import org.antlr.runtime.tree.ParseTree
+import elanTele.ir.values.classes.*
+import elanTele.parser.ElanTeleLexer
+import elanTele.parser.ElanTeleParser
+import org.antlr.v4.runtime.Token
+import org.antlr.v4.runtime.tree.ParseTree
 
 object ValueInterpreter {
     /*
@@ -15,15 +18,41 @@ object ValueInterpreter {
         | LPAREN expression RPAREN
         ;
     */
-    fun getValue(tree: ParseTree): Value {
-        val valueString = tree.getChild(0).text
-
-        // ValueInterpreter should only be called on terminals
-        if (valueString.startsWith("(")) {
-            // TODO create a new exception
-            throw Exception("ValueInterpreter called on (expression)")
+    fun getValue(tree: ElanTeleParser.PrimaryContext): Value = tree.payload.let { node ->
+        when (node) {
+            is ElanTeleParser.LiteralContext -> getLiteral(tree.literal())
+            is ElanTeleParser.FunctionLiteralContext -> TODO("Parse function")
+            else -> TODO("handle unexpected node")
         }
+    }
 
-        return IntegerValue(42)
+    private fun getLiteral(tree: ElanTeleParser.LiteralContext): Value = tree.payload.let { node ->
+        when (node) {
+            is ElanTeleParser.TupleContext -> interpretTuple(node)
+            is ElanTeleParser.ArrayContext -> interpretArray(node)
+            is ElanTeleParser.LineStringLiteralContext -> StringValue(node.text!!) // node.text probably has ""
+            is Token -> when (node.type) {
+//                ElanTeleLexer.IntegerLiteral -> IntegerValue(node.text.toInt())
+//                ElanTeleLexer.RealLiteral -> RealValue(node.text.toDouble())
+//                ElanTeleLexer.BooleanLiteral -> BooleanValue(node.text!!.toBoolean())
+//                ElanTeleLexer.EmptyType -> EmptyValue()
+                else -> TODO()
+            }
+            else -> {
+                throw Exception("The tree has unknown LiteralContext payload: $tree")
+            }
+        }
+    }
+
+    private fun interpretTuple(tupleContext: ElanTeleParser.TupleContext): DictValue {
+        TODO("Implement tuple interpreter")
+    }
+
+    private fun interpretArray(arrayContext: ElanTeleParser.ArrayContext): ArrayValue {
+        TODO("Implement array interpreter")
+    }
+
+    private fun interpretLineString(lineStringLiteralContext: ElanTeleParser.LineStringLiteralContext): StringValue {
+        TODO("Implement LineString interpreter")
     }
 }
