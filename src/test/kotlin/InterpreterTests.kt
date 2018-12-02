4de2b169be10d3aa71af725619ae510d9a029102
novo <ecat3@yandex.ru>
2018-12-02 21:14:49 +0300

    test generator implementation

--- src/test/kotlin/InterpreterTests.kt
+++ src/test/kotlin/InterpreterTests.kt
@@ -1,6 +1,11 @@
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import elanTele.ElanTeleSyntaxTreeGenearator
+import elanTele.interpreter.statements.StatementsSequenceInterpreter
+import elanTele.parser.ElanTeleLexer
+import elanTele.parser.ElanTeleParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 import org.junit.jupiter.api.*
 import java.io.File
 
@@ -16,7 +21,12 @@ class InterpreterTests {
     @TestFactory
     fun testTreeGeneration() = testFiles.map { inputFile ->
         DynamicTest.dynamicTest(inputFile.nameWithoutExtension) {
-            inputFile.readText()
+            //inputFile.readText()
+            val lexer = ElanTeleLexer(CharStreams.fromPath(inputFile.toPath()))
+            val parser = ElanTeleParser(CommonTokenStream(lexer))
+            val program = parser.program()
+            val statements = StatementsSequenceInterpreter.getStatementSequence(program)
+            Assertions.assertNotNull(statements)
         }
     }
 }
