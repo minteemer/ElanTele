ff01a93be8f711eedbe40d5e34610a171e2980c8
Ilgizar <ilgizar1313@gmail.com>
2018-12-02 15:58:34 +0300

    Expressions Interpreters

---
+++ src/main/kotlin/elanTele/interpreter/expressions/ReadExpressionInterpreter.kt
@@ -0,0 +1,12 @@
+package elanTele.interpreter.expressions
+
+import elanTele.ir.expressions.Expression
+import org.antlr.v4.runtime.tree.ParseTree
+
+object ReadExpressionInterpreter {
+
+    fun getReadExpression(tree: ParseTree): Expression {
+        TODO("Implement declaration parsing")
+    }
+
+}
---
+++ src/main/kotlin/elanTele/interpreter/expressions/ReferenceExpressionInterpreter.kt
@@ -0,0 +1,12 @@
+package elanTele.interpreter.expressions
+
+import elanTele.ir.expressions.Expression
+import org.antlr.v4.runtime.tree.ParseTree
+
+object ReferenceExpressionInterpreter {
+
+    fun getReferenceExpression(tree: ParseTree): Expression {
+        TODO("Implement declaration parsing")
+    }
+
+}
