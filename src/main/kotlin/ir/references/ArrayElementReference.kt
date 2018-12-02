186171db6e8f348a82f77da2a899fffa4b571e8f
Timur <minteemer@gmail.com>
2018-12-02 12:09:52 +0300

    References for Arrays, Dics, Functions and Variables

---
+++ src/main/kotlin/ir/references/ArrayElementReference.kt
@@ -0,0 +1,37 @@
+package ir.references
+
+import ir.Context
+import ir.expressions.Expression
+import ir.values.ArrayValue
+import ir.values.IntegerValue
+import ir.values.Value
+
+class ArrayElementReference(
+        val arrayReference: Reference,
+        val indexExpression: Expression
+) : Reference {
+
+    override fun setValue(context: Context, value: Value) {
+        val array = arrayReference.getValue(context)
+        if (array is ArrayValue) {
+            val index = indexExpression.execute(context)
+            if (index is IntegerValue) {
+                array.setElement(index.value, value)
+            } else
+                throw Exception() // TODO: handle non-integer index
+        } else
+            throw Exception() // TODO: handle non-array type
+    }
+
+    override fun getValue(context: Context): Value {
+        val array = arrayReference.getValue(context)
+        if (array is ArrayValue) {
+            val index = indexExpression.execute(context)
+            if (index is IntegerValue) {
+                return array.getElement(index.value)
+            } else
+                throw Exception() // TODO: handle non-integer index
+        } else
+            throw Exception() // TODO: handle non-array type
+    }
+}
--- src/main/kotlin/ir/values/ArrayValue.kt
+++ src/main/kotlin/ir/values/ArrayValue.kt
@@ -1,12 +1,27 @@
 package ir.values
 
-class ArrayValue(val values: List<Value>) : Value(ValueClass.ARRAY){
+class ArrayValue(values: Map<Int, Value>) : Value(ValueClass.ARRAY) {
+
+    constructor(values: List<Value>) : this(
+            values
+                    .mapIndexed { index, value -> index to value }
+                    .associate { it }
+    )
+
+    private val values = HashMap(values)
+
+    fun getElement(index: Int): Value = values[index] ?: EmptyValue()
+
+    fun setElement(index: Int, value: Value) {
+        values[index] = value
+    }
 
     override fun add(other: Value): Value = when (other) {
-        is ArrayValue -> ArrayValue(values + other.values)
+        is ArrayValue -> ArrayValue(values + other.values) // TODO: concatenate sparse arrays
         else -> super.add(other)
     }
 
-    override fun toString(): String = values.joinToString(prefix = "[", postfix = "]")
+    override fun toString(): String = values.entries
+            .joinToString(prefix = "[", postfix = "]") { (index, value) -> "$index: $value" }
 
 }
