cca10b933967e7b08c3f9f2d197bde542fa80f15
novo <ecat3@yandex.ru>
2018-12-02 13:36:12 +0300

    better exception in IfStatement

--- src/main/kotlin/ir/statements/IfStatement.kt
+++ src/main/kotlin/ir/statements/IfStatement.kt
@@ -2,6 +2,7 @@ package ir.statements
 
 import ir.expressions.Expression
 import ir.Context
+import ir.references.InvalidVariableTypeException
 import ir.values.BooleanValue
 
 class IfStatement(
@@ -19,6 +20,6 @@ class IfStatement(
             else
                 elseBody?.executeAll(newContext)
         } else
-            throw Exception("Expected boolean value for \"if\" condition") // TODO: throw better exception
+            throw InvalidVariableTypeException("Expected boolean for \"if\", got ${condition.javaClass.simpleName} ")
     }
 }
