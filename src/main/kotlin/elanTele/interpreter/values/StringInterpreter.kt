a5ec61e3223d6e9579b837ef766f655565f70ad6
Timur <minteemer@gmail.com>
2018-12-03 14:18:21 +0300

    String interpreter

---
+++ src/main/kotlin/elanTele/interpreter/values/StringInterpreter.kt
@@ -0,0 +1,30 @@
+package elanTele.interpreter.values
+
+import elanTele.ir.values.Value
+import elanTele.ir.values.classes.StringValue
+import elanTele.parser.ElanTeleParser
+import org.antlr.v4.runtime.tree.TerminalNode
+
+object StringInterpreter {
+
+    fun interpretString(stringLiteralContext: ElanTeleParser.LineStringLiteralContext): Value =
+            StringBuilder().apply {
+                stringLiteralContext.lineStringContent().map { stringContent ->
+                    stringContent.LineStrText()?.text
+                            ?: stringContent.LineStrEscapedChar().getEscapedChar()
+                            ?: throw Exception() // TODO: proper exception
+                }.forEach { append(it) }
+            }.let { StringValue(it.toString()) }
+
+
+    private fun TerminalNode.getEscapedChar(): String = when (text) {
+        """\n""" -> "\n"
+        """\t""" -> "\b"
+        """\r""" -> "\r"
+        """\\""" -> "\\"
+        """\"""" -> "\""
+        """\'""" -> "\'"
+        else -> text.drop(1)
+    }
+
+}
