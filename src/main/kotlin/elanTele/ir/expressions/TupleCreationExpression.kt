e4336bac5e217466b40d1377389039da5f85a7f0
Timur <minteemer@gmail.com>
2018-12-03 19:45:27 +0300

    Dicts converted to tuples, access tuple elements by index

--- src/main/kotlin/elanTele/ir/expressions/TupleCreationExpression.kt
+++ src/main/kotlin/elanTele/ir/expressions/TupleCreationExpression.kt
@@ -2,11 +2,11 @@ package elanTele.ir.expressions
 
 import elanTele.ir.Context
 import elanTele.ir.values.Value
-import elanTele.ir.values.classes.DictValue
+import elanTele.ir.values.classes.TupleValue
 
 class TupleCreationExpression(private val expressions: Map<String, Expression>):Expression {
 
     override fun execute(context: Context): Value =
-            DictValue(expressions.mapValues { (key, value) -> value.execute(context) })
+            TupleValue(expressions.mapValues { (key, value) -> value.execute(context) })
 
 }
---
+++ src/main/kotlin/elanTele/ir/references/TupleElementIdentifierReference.kt
@@ -0,0 +1,32 @@
+package elanTele.ir.references
+
+import elanTele.ir.Context
+import elanTele.ir.exceptions.InvalidTypeException
+import elanTele.ir.exceptions.UnresolvedIdentifierException
+import elanTele.ir.values.Value
+import elanTele.ir.values.classes.TupleValue
+
+data class TupleElementIdentifierReference(
+        val arrayReference: Reference,
+        val identifier: String
+) : Reference {
+
+    override fun setValue(context: Context, value: Value) {
+        val dict = arrayReference.getValue(context)
+        if (dict is TupleValue) {
+            dict.setElement(identifier, value)
+        } else {
+            throw InvalidTypeException("Expected ArrayValue, got ${dict.javaClass.simpleName}")
+        }
+    }
+
+    override fun getValue(context: Context): Value {
+        val dict = arrayReference.getValue(context)
+        if (dict is TupleValue) {
+            return dict.getElement(identifier)
+                    ?: throw UnresolvedIdentifierException("Unresolved identifier: $dict has no $identifier")
+        } else {
+            throw InvalidTypeException("Expected ArrayValue, got ${dict.javaClass.simpleName}")
+        }
+    }
+}
