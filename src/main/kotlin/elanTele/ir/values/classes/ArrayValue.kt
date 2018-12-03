24a64c264db1381a8fcc69668c4cab90fad47ac2
Timur <minteemer@gmail.com>
2018-12-04 02:54:00 +0300

    Array indexes starting from 0, REPL and source file selection from command line

--- src/main/kotlin/elanTele/ir/values/classes/ArrayValue.kt
+++ src/main/kotlin/elanTele/ir/values/classes/ArrayValue.kt
@@ -2,6 +2,7 @@ package elanTele.ir.values.classes
 
 import elanTele.ir.values.Value
 import elanTele.ir.values.ValueClass
+import java.lang.IndexOutOfBoundsException
 
 class ArrayValue(values: Map<Int, Value>) : Value(ValueClass.ARRAY) {
 
@@ -16,6 +17,8 @@ class ArrayValue(values: Map<Int, Value>) : Value(ValueClass.ARRAY) {
     fun getElement(index: Int): Value = values[index] ?: EmptyValue()
 
     fun setElement(index: Int, value: Value) {
+        if (index !in 1..Int.MAX_VALUE)
+            throw IndexOutOfBoundsException("Array indexes must be positive non-zero integers")
         values[index] = value
     }
 
