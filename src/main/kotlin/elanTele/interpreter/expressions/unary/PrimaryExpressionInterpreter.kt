de2adf84106b07c09b4cde3483bb745f1c147373
Nikolay Matyashov <lave0chance@gmail.com>
2018-12-03 16:14:14 +0300

    Array Literal Parsing Added

--- src/main/kotlin/elanTele/interpreter/expressions/unary/PrimaryExpressionInterpreter.kt
+++ src/main/kotlin/elanTele/interpreter/expressions/unary/PrimaryExpressionInterpreter.kt
@@ -3,15 +3,22 @@ package elanTele.interpreter.expressions.unary
 import elanTele.interpreter.expressions.binary.ExpressionInterpreter
 import elanTele.interpreter.values.FunctionLiteralInterpreter
 import elanTele.interpreter.values.LiteralInterpreter
+import elanTele.ir.expressions.ArrayCreationExpression
 import elanTele.ir.expressions.Expression
 import elanTele.ir.expressions.ReadExpression
 import elanTele.ir.expressions.ValueExpression
+import elanTele.ir.values.classes.ArrayValue
+import elanTele.ir.values.classes.DictValue
 import elanTele.parser.ElanTeleParser
 
 object PrimaryExpressionInterpreter {
 
     fun getPrimaryExpression(tree: ElanTeleParser.PrimaryContext): Expression =
-            tree.literal()?.let { ValueExpression(LiteralInterpreter.getLiteral(it)) }
+            tree.literal()?.let {
+                it.array()?.let { interpretArray(it) }
+                        //?: it.tuple()?.let { interpretTuple(it) }
+                        ?: ValueExpression(LiteralInterpreter.getLiteral(it))
+            }
                     ?: tree.functionLiteral()?.let { ValueExpression(FunctionLiteralInterpreter.getFunction(it)) }
                     ?: tree.expression()?.let { ExpressionInterpreter.getExpression(it) }
                     ?: tree.ReadInt()?.let { ReadExpression(ReadExpression.InputType.INTEGER) }
@@ -19,4 +26,14 @@ object PrimaryExpressionInterpreter {
                     ?: tree.ReadReal()?.let { ReadExpression(ReadExpression.InputType.REAL) }
                     ?: throw ClassCastException("Unknown tree element")
 
+    private fun interpretTuple(tupleContext: ElanTeleParser.TupleContext): DictValue {
+        TODO("Implement tuple interpreter. Received tuple context: $tupleContext")
+    }
+
+    private fun interpretArray(arrayContext: ElanTeleParser.ArrayContext): ArrayCreationExpression =
+            ArrayCreationExpression(
+                    arrayContext.expression().mapIndexed { index, expressionContext ->
+                        index to ExpressionInterpreter.getExpression(expressionContext)
+                    }.associate { it }
+            )
 }
--- src/main/kotlin/elanTele/interpreter/values/LiteralInterpreter.kt
+++ src/main/kotlin/elanTele/interpreter/values/LiteralInterpreter.kt
@@ -1,5 +1,8 @@
 package elanTele.interpreter.values
 
+import elanTele.interpreter.expressions.binary.ExpressionInterpreter
+import elanTele.ir.expressions.ArrayCreationExpression
+import elanTele.ir.expressions.Expression
 import elanTele.ir.values.Value
 import elanTele.ir.values.classes.*
 import elanTele.parser.ElanTeleParser
@@ -8,23 +11,11 @@ import elanTele.parser.ElanTeleParser
 object LiteralInterpreter {
 
     fun getLiteral(tree: ElanTeleParser.LiteralContext): Value = with(tree) {
-        tuple()?.let { interpretTuple(it) }
-                ?: array()?.let { interpretArray(it) }
-                ?: lineStringLiteral()?.let { StringInterpreter.interpretString(it) }
+        lineStringLiteral()?.let { StringInterpreter.interpretString(it) }
                 ?: IntegerLiteral()?.let { IntegerValue(it.text.toInt()) }
                 ?: RealLiteral()?.let { RealValue(it.text.toDouble()) }
                 ?: BooleanLiteral()?.let { BooleanValue(it.text!!.toBoolean()) }
                 ?: EmptyType()?.let { EmptyValue() }
                 ?: throw Exception("The tree has unknown LiteralContext payload: $tree")
     }
-
-
-    private fun interpretTuple(tupleContext: ElanTeleParser.TupleContext): DictValue {
-        TODO("Implement tuple interpreter. Received tuple context: $tupleContext")
-    }
-
-    private fun interpretArray(arrayContext: ElanTeleParser.ArrayContext): ArrayValue {
-        TODO("Implement array interpreter. Received array context: $arrayContext")
-    }
-
 }
