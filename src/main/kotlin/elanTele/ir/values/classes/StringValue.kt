b79b65b1a6d321ef9a5663478bd8a4f88c0055d4
Ilgizar <ilgizar1313@gmail.com>
2018-12-04 20:09:14 +0300

    additional comments + Lexer fixed

--- src/main/kotlin/elanTele/ir/values/classes/StringValue.kt
+++ src/main/kotlin/elanTele/ir/values/classes/StringValue.kt
@@ -5,13 +5,28 @@ import elanTele.ir.values.ValueClass
 
 class StringValue(val value: String) : Value(ValueClass.STRING) {
 
+    /**
+     *  @param other string that will be concatenated with current string
+     *  @return [StringValue] that is concat of current and other strings
+     *  @throws [UnresolvedOperatorException] if other is not of type [StringValue]
+     *
+     */
     override fun add(other: Value): Value = when (other) {
         is StringValue -> StringValue(value + other.value)
         else -> super.add(other)
     }
 
+    /**
+     *  @return [String] representation of current string
+     *
+     */
     override fun toString(): String = value
 
+    /**
+     *  @return true if current string equal to other, false otherwise
+     *  @throws [UnresolvedOperatorException] if other is not of type [StringValue]
+     *
+     */
     override fun equals(other: Any?): Boolean =
             when (other) {
                 is StringValue -> other.value == value
