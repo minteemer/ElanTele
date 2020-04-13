dcf359002f5b91c1bfa04b4258dff0b1f24c4eb8
Timur <minteemer@gmail.com>
2020-04-13 19:38:34 +0300

    Blackbox tests

---
+++ src/test/kotlin/ExceptionTests.kt
@@ -0,0 +1,86 @@
+import elanTele.ElanTeleExecutor
+import elanTele.ir.Context
+import elanTele.ir.exceptions.*
+import elanTele.parser.ElanTeleLexer
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.misc.ParseCancellationException
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+
+class ExceptionTests {
+    private data class ExceptionTest<E>(
+            val name: String,
+            val sourceCode: String,
+            val exceptionClass: Class<E>
+    )
+
+    companion object {
+        private val exceptionTests = listOf(
+                ExceptionTest(
+                        "Assignment",
+                        "var f := func is print 1; end; f() := 1",
+                        AssignmentException::class.java
+                ),
+                ExceptionTest(
+                        "Context",
+                        "a := 1;",
+                        ContextException::class.java
+                ),
+                ExceptionTest(
+                        "Array 0 index",
+                        "var a := [1,2,3]; print a[0]",
+                        InvalidIndexException::class.java
+                ),
+                ExceptionTest(
+                        "InvalidIndex",
+                        "var a := {a := 1, b := 2}; print a.3",
+                        InvalidIndexException::class.java
+                ),
+                ExceptionTest(
+                        "InvalidType",
+                        "var a := 1; print a.1",
+                        InvalidTypeException::class.java
+                ),
+                ExceptionTest(
+                        "UniterableRange",
+                        "var a := func is print 1; end; for i in a..1 loop print i; end;",
+                        UniterableRangeException::class.java
+                ),
+                ExceptionTest(
+                        "UnresolvedIdentifier",
+                        "var a := {a := 1, b := 2}; print a.c",
+                        UnresolvedIdentifierException::class.java
+                ),
+                ExceptionTest(
+                        // ExpressionException is unreachable
+                        "Expression",
+                        "1",
+                        Exception::class.java
+                ),
+                ExceptionTest(
+                        "UnresolvedOperator",
+                        "var f := func is print 1; end; print f + 1",
+                        UnresolvedOperatorException::class.java
+                ),
+                ExceptionTest(
+                        "ParseCancellation",
+                        "print )1+1(",
+                        ParseCancellationException::class.java
+                )
+        )
+    }
+
+    @TestFactory
+    fun exceptionTestsGenerator() = exceptionTests.map { test ->
+        DynamicTest.dynamicTest(test.name) {
+            val lexer = ElanTeleLexer(CharStreams.fromString(test.sourceCode))
+            val context = Context()
+            Assertions.assertThrows(test.exceptionClass) {
+                ElanTeleExecutor.unsafeExecute(context, lexer)
+                // uncomment if needed
+                // println("Result context: $context")
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/InterpreterTests.kt
@@ -0,0 +1,73 @@
+import elanTele.interpreter.ProgramInterpreter
+import elanTele.ir.Context
+import elanTele.parser.ElanTeleLexer
+import elanTele.parser.ElanTeleParser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.junit.jupiter.api.*
+import java.io.File
+
+class InterpreterTests {
+    companion object {
+        private val classLoader = InterpreterTests::class.java.classLoader
+
+        private val trivialFiles: List<File> = classLoader.getResource("trivial_programs")
+                ?.let { File(it.file).listFiles() }
+                ?.filter { it.isFile }
+                ?: emptyList()
+
+        private val incorrectFiles: List<File> = classLoader.getResource("incorrect_programs")
+                ?.let { File(it.file).listFiles() }
+                ?.filter { it.isFile }
+                ?: emptyList()
+
+        private val algorithmsFiles: List<File> = classLoader.getResource("algorithms")
+                ?.let { File(it.file).listFiles() }
+                ?.filter { it.isFile }
+                ?: emptyList()
+    }
+
+
+    @TestFactory
+    fun trivialTestsGenerator() = trivialFiles.map { inputFile ->
+        DynamicTest.dynamicTest(inputFile.nameWithoutExtension) {
+            val lexer = ElanTeleLexer(CharStreams.fromPath(inputFile.toPath()))
+            val parser = ElanTeleParser(CommonTokenStream(lexer))
+            val program = parser.program()
+            val statements = ProgramInterpreter.getProgram(program)
+
+            val context = Context()
+            statements.execute(context)
+            println("Result context: $context")
+        }
+    }
+
+    @TestFactory
+    fun incorrectTestsGenerator() = incorrectFiles.map { inputFile ->
+        DynamicTest.dynamicTest(inputFile.nameWithoutExtension) {
+            val lexer = ElanTeleLexer(CharStreams.fromPath(inputFile.toPath()))
+            val parser = ElanTeleParser(CommonTokenStream(lexer))
+            val program = parser.program()
+            val statements = ProgramInterpreter.getProgram(program)
+
+            val context = Context()
+            statements.execute(context)
+            println("Result context: $context")
+        }
+    }
+
+    @TestFactory
+    fun algorithmsTestsGenerator() = algorithmsFiles.map { inputFile ->
+        DynamicTest.dynamicTest(inputFile.nameWithoutExtension) {
+            val lexer = ElanTeleLexer(CharStreams.fromPath(inputFile.toPath()))
+            val parser = ElanTeleParser(CommonTokenStream(lexer))
+            val program = parser.program()
+            val statements = ProgramInterpreter.getProgram(program)
+
+            val context = Context()
+            statements.execute(context)
+            println("Result context: $context")
+        }
+    }
+
+}
